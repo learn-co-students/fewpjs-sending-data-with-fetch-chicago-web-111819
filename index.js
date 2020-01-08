ce09d8d9e564dc5324dbc5c9234b04464194ef16
github username <github email address>
2020-01-08 16:55:42 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function submitData(name, email){
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+    return fetch("http://localhost:3000/users", configObj)
+        .then(resp => resp.json())
+        .then(json => document.body.innerHTML = json.id)
+        .catch(error => document.body.innerHTML = 'Unauthorized Access')
+}
