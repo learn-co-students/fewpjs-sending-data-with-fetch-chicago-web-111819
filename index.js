6ef49340db12fc7b2d5e0802deb9b007c69a07bb
hfarnsworth <havilahfarnsworth@gmail.com>
2020-01-09 21:02:58 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+// // Add your code here
+
+function submitData(name, email) {
+
+    
+    let formData = {
+        "name": name,
+        "email": email
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(response =>
+            response.json()
+        )
+        .then(object => document.querySelector('body').innerHTML = object.id)
+        .catch(putErrorInBody)
+
+}
+
+
+
+function putErrorInBody(error) {
+    const body = document.querySelector('body')
+    const ptag = document.createElement('p')
+    ptag.innerHTML = error.message
+    body.append(ptag)
+}
