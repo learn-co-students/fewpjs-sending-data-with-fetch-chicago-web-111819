695764831e05ab9ecf312c89f89299dddfc498a7
Jonathan <github email address>
2020-01-13 21:40:44 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+let formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle"
+  };
+   
+  // method: "POST" is missing from the object below
+  let configObj = {
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+   
+  fetch("http://localhost:3000/dogs", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
